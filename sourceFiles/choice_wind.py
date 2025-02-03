c2af08e0f3f2aeb7ee1a181e449e8f22f8583022
Sofi <116519741+sofilukash@users.noreply.github.com>
2025-02-03 10:20:27 +0200

    Update choice_wind.py

--- sourceFiles/choice_wind.py
+++ sourceFiles/choice_wind.py
@@ -1,5 +1,6 @@
 import os
 import sys
+
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_choice_wind(object):
@@ -28,8 +29,10 @@ class Ui_choice_wind(object):
         icon_path = os.path.join(project_dir, 'logo.png')
         icon.addPixmap(QtGui.QPixmap(icon_path), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         choice_wind.setWindowIcon(icon)
+
         self.centralwidget = QtWidgets.QWidget(choice_wind)
         self.centralwidget.setObjectName('centralwidget')
+
         self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
         self.textBrowser.setGeometry(QtCore.QRect(70, 20, 550, 150))
         font = QtGui.QFont()
@@ -40,6 +43,7 @@ class Ui_choice_wind(object):
         self.textBrowser.setFrameShape(QtWidgets.QFrame.NoFrame)
         self.textBrowser.setFrameShadow(QtWidgets.QFrame.Plain)
         self.textBrowser.setObjectName('textBrowser')
+
         self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_4.setGeometry(QtCore.QRect(50, 15, 100, 34))
         font = QtGui.QFont()
@@ -51,6 +55,7 @@ class Ui_choice_wind(object):
         self.pushButton_4.setStyleSheet('color: rgb(167, 194, 196);\nbackground-color: rgb(0, 0, 0);')
         self.pushButton_4.setObjectName('pushButton')
         self.pushButton_4.clicked.connect(self.back_to_main_window)
+
         self.textBrowser_2 = QtWidgets.QTextBrowser(self.centralwidget)
         self.textBrowser_2.setGeometry(QtCore.QRect(440, 380, 250, 80))
         font = QtGui.QFont()
@@ -62,6 +67,7 @@ class Ui_choice_wind(object):
         self.textBrowser_2.setFrameShape(QtWidgets.QFrame.NoFrame)
         self.textBrowser_2.setFrameShadow(QtWidgets.QFrame.Plain)
         self.textBrowser_2.setObjectName('textBrowser_3')
+
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(10, 90, 690, 40))
         font = QtGui.QFont()
@@ -71,6 +77,7 @@ class Ui_choice_wind(object):
         self.pushButton.setStyleSheet('color: rgb(167, 194, 196);\nbackground-color: rgb(15, 71, 98);')
         self.pushButton.setObjectName('pushButton')
         self.pushButton.clicked.connect(self.get_open_new_rule_window)
+
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_2.setGeometry(QtCore.QRect(10, 160, 690, 40))
         font = QtGui.QFont()
@@ -80,6 +87,7 @@ class Ui_choice_wind(object):
         self.pushButton_2.setStyleSheet('color: rgb(167, 194, 196);\nbackground-color: rgb(15, 71, 98);')
         self.pushButton_2.setObjectName('pushButton_2')
         self.pushButton_2.clicked.connect(self.get_open_removal_window)
+
         self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_3.setGeometry(QtCore.QRect(10, 230, 690, 40))
         font = QtGui.QFont()
@@ -89,22 +97,30 @@ class Ui_choice_wind(object):
         self.pushButton_3.setStyleSheet('color: rgb(167, 194, 196);\nbackground-color: rgb(15, 71, 98);')
         self.pushButton_3.setObjectName('pushButton_3')
         self.pushButton_3.clicked.connect(self.get_open_extract_window)
+
         choice_wind.setCentralWidget(self.centralwidget)
+
         self.menubar = QtWidgets.QMenuBar(choice_wind)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 447, 18))
         self.menubar.setObjectName('menubar')
+
         choice_wind.setMenuBar(self.menubar)
+
         self.statusbar = QtWidgets.QStatusBar(choice_wind)
         self.statusbar.setObjectName('statusbar')
         choice_wind.setStatusBar(self.statusbar)
+
         self.retranslateUi(choice_wind)
         QtCore.QMetaObject.connectSlotsByName(choice_wind)
 
     def retranslateUi(self, choice_wind):
         _translate = QtCore.QCoreApplication.translate
         choice_wind.setWindowTitle(_translate('choice_wind', 'choice_wind'))
-        self.textBrowser.setHtml(_translate('choice_wind', '<html><body><p align="center">Оберіть дію</p></body></html>'))
-        self.textBrowser_2.setHtml(_translate('choice_wind', '<html><body><p align="right">SecureCloudAutoBackupProject<br>by well-wisher</p></body></html>'))
+        self.textBrowser.setHtml(_translate('choice_wind', '<html><body><p align="center">'
+                                                           'Оберіть дію</p></body></html>'))
+        self.textBrowser_2.setHtml(_translate('choice_wind', '<html><body><p align="right">'
+                                                             'SecureCloudAutoBackupProject<br>'
+                                                             'by well-wisher</p></body></html>'))
         self.pushButton.setText(_translate('choice_wind', 'Створити нове правило для резервного копіювання'))
         self.pushButton_2.setText(_translate('choice_wind', 'Переглянути або видалити наявні правила резервного копіювання'))
         self.pushButton_3.setText(_translate('choice_wind', 'Видобути дані з реервної копії'))
@@ -134,7 +150,8 @@ class Ui_choice_wind(object):
         self.main_wind.show()
         self.close_this_window()
 
-    def close_this_window(self):
+    @staticmethod
+    def close_this_window():
         main_window = QtWidgets.QApplication.activeWindow()
         if main_window:
             main_window.close()
@@ -153,3 +170,4 @@ if __name__ == '__main__':
     ui.setupUi(choice_wind)
     choice_wind.show()
     sys.exit(app.exec_())
+    
